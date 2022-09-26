5f2d551c49f8e44799573d56ca2e25730aa13873
Haebin <joker7011@naver.com>
2022-09-26 16:00:50 +0900

    2022/09/26 16:00

--- 0x02_10871.c
+++ 0x02_10871.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-	int N, X, res;
+	int res, N, X;
 	scanf("%d %d",&N, &X);
 	for (int i = 0; i < N; i++)
 	{
 		scanf("%d", &res);
 		if(res < X)
-			printf("%d", res);
+			printf("%d ", res);
 	}
 }
+
+// 맞았습니다!!	1112kb	4ms
