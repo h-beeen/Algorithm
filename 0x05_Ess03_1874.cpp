97575a7a4106fec656f86285808cda011299b2b5
Haebin <joker7011@naver.com>
2022-09-28 17:14:30 +0900

    2022/09/28 17:14

---
+++ 0x05_Ess03_1874.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main (void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	
+}
---
+++ 0x05_Ess04_2493.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stack>
+#include <utility>
+
+using namespace std;
+
+int N;
+stack<pair<int, int>> tower; //2개의 데이터를 저장할 수 있는 변수
+
+
