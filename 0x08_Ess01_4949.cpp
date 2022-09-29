cf3ba546af6497df9355bbc0077d0c45ea97a41d
Haebin <joker7011@naver.com>
2022-09-29 15:14:13 +0900

    2022/09/29 15:14

--- 0x08_Ess01_4949.cpp
+++ 0x08_Ess01_4949.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 #include <stack>
 
 using namespace std;
@@ -10,17 +9,34 @@ int main (void)
 	cin.tie(0);
 	cout.tie(0);
 
-	while(1)
+	string s;
+	getline(cin, s);
+	
+	stack <char> a;
+
+	int flag = 1;
+	int i = 0;
+
+	while(s[i])
 	{
-		int isValid = 1;
-		string a;
+		if(s[i] == '(' || s[i] == '[')
+			a.push(s[i]);
+		i++;
+	}	
+		if(s[i] == '(' || s[i] == '[') 			// (, [ 입력시
+			a.push(s[i]);						// 스택에 저장
 
-		getline(cin, a);
-		if(a == ".")
-			break;
+		else if(s[i] == ')')					// ) 입력시
+		{
+			if(!a.empty() && a.top() == '(')	// 근데 top 스택이 (이라면
+				a.pop();
 
-		stack <char> st;
-		
-		cout << a; 
+			else								// ) 입력시  top 스택이 (이 아니면?
+			{
+				flag = 0;						// flag = false
+				break;							// while break;
+			}
+		}
 	}
 }
+
