9fe2232c70f75353d332a0925b311c09d77596fc
Haebin <joker7011@naver.com>
2022-09-30 09:53:39 +0900

    2022/09/30 09:53

--- 0x08_Ess01_4949.cpp
+++ 0x08_Ess01_4949.cpp
@@ -17,26 +17,4 @@ int main (void)
 	int flag = 1;
 	int i = 0;
 
-	while(s[i])
-	{
-		if(s[i] == '(' || s[i] == '[')
-			a.push(s[i]);
-		i++;
-	}	
-		if(s[i] == '(' || s[i] == '[') 			// (, [ 입력시
-			a.push(s[i]);						// 스택에 저장
-
-		else if(s[i] == ')')					// ) 입력시
-		{
-			if(!a.empty() && a.top() == '(')	// 근데 top 스택이 (이라면
-				a.pop();
-
-			else								// ) 입력시  top 스택이 (이 아니면?
-			{
-				flag = 0;						// flag = false
-				break;							// while break;
-			}
-		}
-	}
 }
-
