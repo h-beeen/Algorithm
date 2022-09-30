46cd5e3c1e691a33d6a1b733a29837b5648073f2
Haebin <joker7011@naver.com>
2022-09-30 10:57:32 +0900

    2022/09/30 10:57

--- 0x08_Ess01_4949.cpp
+++ 0x08_Ess01_4949.cpp
@@ -9,12 +9,67 @@ int main (void)
 	cin.tie(0);
 	cout.tie(0);
 
+	while(true)
+	{
 	string s;
+	stack <char> a;
+
 	getline(cin, s);
 	
-	stack <char> a;
+	if(s[0] == '.' && s[1] == '\0')
+	{
+		cout << "yes" << '\n';
+		break;
+	}
 
 	int flag = 1;
 	int i = 0;
 
+	
+		while(s[i])
+		{
+			if(flag == 0)
+				break;
+
+			if(s[i] == '(' || s[i] == '[')	// 만약 여는 괄호가 나온다면
+				a.push(s[i]);				// 해당 괄호 push
+
+			else if(s[i] == ']')
+			{
+				if(a.empty())				// 스택이 비어있는데 닫는 기호가 나온다면
+				{
+					flag = 0;				// Flag = 0
+					break;					// Break;
+				}
+				else if(a.top() == '[')
+					a.pop();
+				else
+				{
+					flag = 0;
+					break;
+				}
+			}
+
+			else if(s[i] == ')')
+			{
+				if(a.empty())				// 스택이 비어있는데 닫는 기호가 나온다면
+				{
+					flag = 0;				// Flag = 0
+					break;					// Break;
+				}
+				else if(a.top() == '(')
+					a.pop();
+				else
+				{
+					flag = 0;
+					break;
+				}
+			}
+			i++;
+		}
+		if(flag && a.empty()) cout << "yes" << '\n';
+		else
+			cout << "no" << '\n';
+	}
+	// 백준에러 Why..?
 }
