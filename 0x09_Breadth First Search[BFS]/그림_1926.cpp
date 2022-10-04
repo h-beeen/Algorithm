4c0740fd33f9aaa6ce6cf8305947eb785803ef0f
h-beeen <joker7011@naver.com>
2022-10-04 23:04:44 +0900

    2022/10/04 23:04

--- 0x09_Breadth First Search[BFS]/그림_1926.cpp
+++ 0x09_Breadth First Search[BFS]/그림_1926.cpp
@@ -1,61 +1,64 @@
 #include <iostream>
 #include <queue>
-
 using namespace std;
 
-#define X first
-#define Y second // pair에서 first, second를 줄여서 쓰기 위해서 사용
+int board[505][505];
+int vis[505][505];
 
-int board[502][502]; // 1이 파란 칸, 0이 빨간 칸에 대응
-bool vis[502][502]; // 해당 칸을 방문했는지 여부를 저장
-int n,m;
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1}; // 상하좌우 네 방향을 의미
+int n, m, area, mx = 0;
+int num = 0;
 
+int main(void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-int main(){
-  ios::sync_with_stdio(0);
-  cin.tie(0);
-  cin >> n >> m;
+	cin >> n >> m;
 
-  for(int i = 0; i < n; i++)
-    for(int j = 0; j < m; j++)
-      cin >> board[i][j]; // 보드 만들고 색칠
+	for(int i = 0; i < n; i++)
+		for(int j = 0; j < m; j++)
+			cin >> board[i][j];						// 1. 보드에 Input Value 저장
 
-  int mx = 0; // 그림의 최댓값
-  int num = 0; // 그림의 수
+	for(int i= 0; i < n; i++)
+	{
+		for(int j = 0; j < m; j++)
+		{
+			if(!(board[i][j]) || vis[i][j])		// 찾을 필요 없는 구역이라면
+				continue;							// 다음 반복문 continue
 
-  for(int i = 0; i < n; i++){
-    for(int j = 0; j < m; j++){ // (i, j)를 시작점으로 하고 싶은 상황
-      if(board[i][j] == 0 || vis[i][j]) continue; 
-	  
-	  // 해당 칸이 색칠이 안된 부분(0)이거나 이미 (i, j)를 방문했을 경우 넘어감
-      // (i,j)는 새로운 그림에 속해있는 시작점
-     
-	  num++; // 그림의 수 1 증가
-      queue<pair<int,int> > Q;
-      vis[i][j] = 1; // (i,j)로 BFS를 시작하기 위한 준비
-      Q.push(make_pair(i,j));
+			num++;									// num = 찾은 시작점의 개수, 즉 그림의 개수				
+			queue<pair<int, int> > q;				// 그 그림의 큐 생성
+			vis[i][j] = 1;						// 나 왔어!
+			q.push(make_pair(i, j));				// 시작점 큐에 Push
 
-      int area = 0; // 그림의 넓이
-      while(!Q.empty()){
-        area++; // 큐에 들어있는 원소를 하나 뺄 때 마다 넓이를 1 증가시킴
-        pair<int,int> cur = Q.front();
-		Q.pop();
+			area = 0;
+			while(!q.empty())						// 큐에 아무 노드도 남지 않을 때 까지 Loop
+			{
+				area++;
+				pair<int, int> temp = q.front();	// 큐의 Front값 pair형으로 저장
+				q.pop();							// 저장 후 Pop
+				
+				for(int dir = 0; dir < 4; dir++)
+				{
+					int nx = temp.first + dx[dir];
+					int ny = temp.second + dy[dir];
 
-        for(int dir = 0; dir < 4; dir++){ // 상하좌우 칸을 살펴볼 것이다.
-          int nx = cur.X + dx[dir];
-          int ny = cur.Y + dy[dir]; // nx, ny에 dir에서 정한 방향의 인접한 칸의 좌표가 들어감
-          if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue; // 범위 밖일 경우 넘어감
-          if(vis[nx][ny] || board[nx][ny] != 1) continue; // 이미 방문한 칸이거나 파란 칸이 아닐 경우
-          vis[nx][ny] = 1; // (nx, ny)를 방문했다고 명시
-          Q.push(make_pair(nx,ny));
-        }
-      }
-      // (i, j)를 시작점으로 하는 BFS가 종료됨
-      mx = max(mx, area); // area가 mx보다 클 경우 mx에 area를 대입. max는 STL에 정의된 함수
-    }
-  }
-  cout << num << '\n' << mx;
+					if(nx < 0 || nx > n || ny < 0 || ny > m)	// 범위 밖 조건
+						continue;								// 다음 반복문으로 continue
+					if(!board[nx][ny] || vis[nx][ny])
+						continue;
+					
+					vis[nx][ny] = 1;
+					q.push(make_pair(nx,ny));
+				}
+			}
+			if(mx <= area)
+				mx = area;
+		}
+	}
+	cout << num << '\n' << mx;
 }
