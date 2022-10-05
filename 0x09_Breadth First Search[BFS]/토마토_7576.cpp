8958f72cae14a3355da52755ac30a7d53ab6d92b
h-beeen <joker7011@naver.com>
2022-10-05 10:53:57 +0900

    2022/10/05 10:53

--- 0x09_Breadth First Search[BFS]/토마토_7576.cpp
+++ 0x09_Breadth First Search[BFS]/토마토_7576.cpp
@@ -3,69 +3,62 @@
 using namespace std;
 
 int n, m;
-int box[1005][1005];
+int board[1005][1005] = {};
 
-int dx[4] = {1,0,-1,0};
-int dy[4] = {0,1,0,-1};
+int dx[4] = {1, 0, -1, 0};
+int dy[4] = {0, 1, 0, -1};
 int result = 0;
 
-int main(void)
+int	main (void)
 {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> n >> m;
-	queue <pair<int,int> > q;
+	cin >> m >> n;
+	queue<pair<int,int> > q;
 
-	// box에 토마토 주입, 익은 토마토(1)이라면 바로 queue push.
-	for(int i = 0; i < n; i++)
+	for(int i = 0; i < n; i++)	
 	{
-		for(int j = 0; j < n; j++)
+		for(int j = 0; j < m; j++)
 		{
-			cin >> box[i][j];
-			if(box[i][j] == 1)
+			cin >> board[i][j];
+			if(board[i][j] == 1)
 				q.push(make_pair(i,j));
 		}
 	}
 
-	// BFS 출발!
 	while(!q.empty())
 	{
-		int x = q.front().first;
-		int y = q.front().second;
+		pair<int,int> temp = q.front();
 		q.pop();
 
-		for(int i = 0; i < 4; i++)
+		for(int dir = 0; dir < 4; dir++)
 		{
-			int nx = x + dx[i];
-			int ny = y + dy[i];
+			int nx = temp.first + dx[dir];
+			int ny = temp.second + dy[dir];
 
-			if(nx>=0 && nx<n && ny>=0 && ny<m && box[nx][ny] == 0)
+			if(nx >= 0 && nx < n && ny >= 0 && ny < m && board[nx][ny] == 0)
 			{
-				box[nx][ny] = box[x][y] + 1;
-				q.push(make_pair(nx,ny));
+				q.push(make_pair(nx, ny));
+				board[nx][ny] = board[temp.first][temp.second] + 1;
 			}
 		}
 	}
-	
-	// 일수 세러 출발!!
-	for(int i =0; i<n; i++)
+
+	for(int i = 0; i < n; i++)	
 	{
-		
-		for(int j = 0; j<m; j++)
+		for(int j = 0; j < m; j++)
 		{
-			// 다 익었니?
-			if(box[i][j] == 0)
+			if(board[i][j] == 0)
 			{
-				cout << -1 << '\n';
+				cout << -1;
 				return 0;
 			}
-			// 그럼 얼마만에 익었니?
-			if(result < box[i][j])
-				result = box[i][j];
+			if(result < board[i][j])
+				result = board[i][j];
 		}
 	}
-	cout << result-1 << '\n';
-	return 0;
+		cout << result -1 << '\n';
+	 
 }
