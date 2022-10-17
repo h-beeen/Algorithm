7a7e4aa2641811374e670fccf6a070db915b53db
h-beeen <joker7011@naver.com>
2022-10-17 15:27:44 +0900

    2022/10/17 15:28

---
+++ BOJ Exam/초콜릿과 나이트 게임_SJ.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int x, y;
+int dx[4] = {-1, 0 ,1, 0};
+int dy[4] = {0, 1, 0, -1};
+
+int	main (void)
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin >> x >> y;
+	
+}
