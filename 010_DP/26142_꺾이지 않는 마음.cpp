4defdc26d43eb3dfd089c4d954065b7e654253d3
Haebin <hbyeon@student.42seoul.kr>
2022-12-02 21:25:38 +0900

    2022/12/02 09:25

--- 010_DP/26142_꺾이지 않는 마음.cpp
+++ 010_DP/26142_꺾이지 않는 마음.cpp
@@ -3,14 +3,13 @@
 #include <algorithm>
 using namespace std;
 
-unsigned long long n, temp_a, temp_b, temp, k2, k3 = 0;
-unsigned long long k1 = 1;
-vector <pair <unsigned long long, unsigned long long> > dr;
-unsigned long long res[10005][10005];
-unsigned long long res2[10005];
+long long n, temp_a, temp_b, temp, k2, k3 = 0;
+vector <pair <long long, long long> > dr;
+long long res[10005][10005];
+long long res2[10005];
 
-bool cmp(pair<unsigned long long, unsigned long long> a,\
-		 pair<unsigned long long, unsigned long long> b)
+bool cmp(pair<long long, long long> a,\
+		 pair<long long, long long> b)
 {
 	if (a.second == b.second)
 		return a.first > b.first;
@@ -38,21 +37,19 @@ int main (void)
 
 	cout << res[0][0] << '\n';
 
-	while (k1 < n)	// k1 = 1;
+	long long k1 = 1;
+	while (k1 < n)	// k1 = 1 ~ n-1;
 	{
 		k2 = 0;		// k2 = 0;
 		temp = 0;
 		k3 = k1;
-		while(k3 > 0 && k2 < n-1)
+		while(k3 >= 0 && k2 < n)
 			temp += res[k3--][k2++];
 		cout << temp << '\n';
 		k1++;
 	}
 
-	cout << res[0][0] << endl;
-	cout << res[1][0] + res[0][1] << endl;
-	cout << res[2][0] + res[1][1] + res[0][2];
-
+	cout << endl;
 	for(int i = 0; i < n; i++) 
 	{
 		cout << '\n';
